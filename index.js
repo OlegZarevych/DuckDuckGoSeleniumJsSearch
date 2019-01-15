d9e9e3ed9868138008070da90c6325ad7925d187
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2019-01-16 00:06:24 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+    var driver = new webdriver.Builder().forBrowser('firefox').build();
+
+    driver.get('http://google.com');
+    
